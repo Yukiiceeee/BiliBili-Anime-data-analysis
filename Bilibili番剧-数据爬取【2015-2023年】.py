78d7f6a44ab4ffd54a5c447120b88cb2b23f719b
Yukiice <13336340366@163.com>
2023-12-09 20:59:00 +0800

    第三次更新

--- Bilibili番剧-数据爬取【2015-2023年】.py
+++ Bilibili番剧-数据爬取【2015-2023年】.py
@@ -1,117 +1,88 @@
+import requests
+import json
 import pandas as pd
-import numpy as np
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import MinMaxScaler
-from numpy import set_printoptions
-import matplotlib.pyplot as plt
-# 设置显示中文字体
-from pylab import mpl
-mpl.rcParams["font.sans-serif"] = ["SimHei"]
-# 设置显示正常符号
-mpl.rcParams["axes.unicode_minus"] = False
-pd.set_option('display.unicode.east_asian_width',True)
 
-# 设置列对齐
-pd.set_option('display.unicode.ambiguous_as_wide',True)
-pd.set_option('display.unicode.east_asian_width',True)
+score_list=[]
+ep_list=[]
+#设置爬取页数
+page=11
+headers = {
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36'
+    }
+url = 'https://api.bilibili.com/pgc/season/index/result'
+for i in range(page):
+        param = {
+            'st': '1',
+            'order': '3',
+            'season_version': '-1',
+            'spoken_language_type': '-1',
+            'area': '-1',
+            'is_finish': '-1',
+            'copyright': '-1',
+            'season_status': '-1',
+            'season_month': '-1',
+            # 可修改year值
+            'year': '[2017,2018)',
+            'style_id': '-1',
+            'sort': '0',
+            'page': i,
+            'season_type': '1',
+            'pagesize': '20',
+            'type': '1',
+        }
+        try:
+                response = requests.get(url=url, params=param, headers=headers)
+                data = json.loads(response.text)
+                json_list = data['data']['list']
+                # 获取ep_id，score
+                for i in range(len(json_list)):
+                    score = json_list[i]['score']
+                    ep_id=json_list[i]['first_ep']['ep_id']
+                    ep_list.append(ep_id)
+                    score_list.append(score)
+        except:
+            print('未爬取到数据')
 
-test= pd.read_excel(io="番剧数据-原始数据【2015-2023】.xlsx",sheet_name="Sheet1")
+# 存储所有数据
+ff=[]
+for i in range(len(ep_list)):
+    ep_id=ep_list[i]
+    # title=title_list[i]
+    score=score_list[i]
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36'
+    }
+    url2='https://api.bilibili.com/pgc/view/web/season?ep_id='+str(ep_id)
 
+    response = requests.get(url=url2, headers=headers)
+    data = json.loads(response.text)
 
-print("============原始前五行数据")
-print(test.head())
+    json_list = data['result']
 
-for x in range(0,test.shape[0]):
-    if ("万" in test['系列追番数'].iloc[x]):
-        df = test['系列追番数'].iloc[x].replace('万系列追番', '').replace('万追番', '').replace('万追剧', '').replace('万系列追剧', '')
-        test.iloc[x,8] = float(df)*100000000
-    else:
-        test.iloc[x,8] = int(test.iloc[x,8].replace('追剧', '').replace('系列追番', '').replace('追番', '').replace('追剧', '').replace('系列追剧', ''))
+    title = json_list['title']
+    views = json_list['stat']['views']
+    likes = json_list['stat']['likes']
+    coins = json_list['stat']['coins']
+    shares = json_list['stat']['share']
+    danmakus = json_list['stat']['danmakus']
+    series_follow = json_list['stat']['follow_text']
+    styles=json_list['styles']
 
+    dic = {
+        '名称': title,
+        '播放量': views,
+        '评分': score,
+        '点赞量': likes,
+        '投币量': coins,
+        '转发量': shares,
+        '弹幕数': danmakus,
+        '系列追番数': series_follow,
+        '所属类别':styles
+    }
 
-# 删掉名称一列
-test.drop(["名称"],axis=1,inplace=True)
-print("\n"+"============数值处理之后数据")
-print(test.head())
-
-# 输出数据清洗前的数据信息
-print("\n"+"============数据清洗前的数据信息")
-test.info()
-
-
-
-#========缺失值处理
-# 将0替换成Nan
-test.replace(to_replace=0,value=np.nan,inplace=True)
-def missvalue_test(data, column, method="Delete"):
-   if (np.all(pd.notnull(data))):
-       print(column+"无缺失值")
-       # return data
-   else:
-       if method == "Delete":
-           print('全部删除'+column+'列缺失值所在行...')
-           print('=' * 70)
-           data.dropna(how = "any",inplace=True)
-           # return data
-       if method == 'fill':
-           print('填补'+column+'列缺失值为列平均值...')
-           data[column].fillna(data[column].mean(), inplace=True)
-           # return data
-
-# 将评分列的缺失值替换成该列的平均值
-missvalue_test(test,"评分","fill")
-# 删除播放量，系列追番数，转发量中的缺失值
-missvalue_test(test,"播放量","Delete")
-missvalue_test(test,"系列追番数","Delete")
-missvalue_test(test,"转发量","Delete")
-missvalue_test(test,"投币量","Delete")
-missvalue_test(test,"点赞量","Delete")
-print("\n"+"============缺失值处理后")
-test.info()
-
-
-# 可视化看数据分布，这里是pandas模块里的画图
-
-test.plot(kind='box',subplots=True,layout=(3,3),sharex=False,figsize = (10,10))
-plt.savefig("箱线图")
-plt.show()
-
-
-# 异常值处理
-#  异常值检验函数 =========================
-def outlier_test(data, column):
-
-       print(f'以 {column} 列为依据，使用 上下截断点法(iqr) 检测异常值...')
-       print('=' * 70)
-       # 四分位点；这里调用函数会存在异常
-       column_iqr = np.quantile(data[column], 0.75) - np.quantile(data[column], 0.25)
-       # 1，3 分位数
-       (q1, q3) = np.quantile(data[column], 0.25), np.quantile(data[column], 0.75)
-       # 计算上下截断点
-       upper, lower = (q3 + 1.5 * column_iqr), (q1 - 1.5 * column_iqr)
-       # 检测异常值
-       outlier = data[(data[column] <= lower) | (data[column] >= upper)]
-       print(f'第一分位数: {q1}, 第三分位数：{q3}, 四分位极差：{column_iqr}')
-       print(f"上截断点：{upper}, 下截断点：{lower}")
-       return outlier, upper, lower
-# 对系列追番数，采取四分位法检测异常值
-outlier, upper, lower = outlier_test(data=test, column="系列追番数")
-
-# 对系列追番数
-# outlier1, upper1, lower1 = outlier_test(data=test, column="系列追番数")
-# outlier = pd.concat([outlier,outlier1],axis=0)
-
-# 输出异常值信息
-print("\n"+"=====================异常值信息")
-outlier.info()
-
-# 删除异常值
-test.drop(index=outlier.index, inplace=True)
-
-# 输出删除异常值后表格信息
-print("\n"+"=====================删除异常值后表格信息")
-test.info()
-
-#保存清洗后数据
-test.to_excel("番剧数据-清洗后数据【2015-2023】.xlsx")
+    ff.append(dic)
+    print(ff)
+    # 利用pd导出表格
+    pd_data = pd.DataFrame(ff)
+    pd_data.to_excel('2017年B站番剧数据.xlsx')
 
